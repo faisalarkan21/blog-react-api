343343bc07cd183eb93c166a8c003fc43faa3f47
faisal arkan <faisalarkan21@gmail.com>
2017-12-11 14:24:26 +0700

    implement SHA-3 /  Keccak 512 hash

---
+++ src/lib/auth.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+import Crypto from 'crypto-js';
+
+const getToken = () => jwt.sign({ coba: 'yeahcoba' }, 'rahasia', { expiresIn: '1h' });
+
+/*
+Author Note :
+https://code.google.com/archive/p/crypto-js/
+
+I made a mistake when I named this implementation SHA-3.
+It should be named Keccak[c=2d]. Each of the SHA-3 functions is based on an instance of the Keccak algorithm,
+which NIST selected as the winner of the SHA-3 competition,
+but those SHA-3 functions won't produce hashes identical to Keccak.
+*/
+
+const getHash = password => Crypto.SHA3(password, { outputLength: 512 }).toString();
+
+export { getToken, getHash };
+
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,7 @@
 import express from 'express';
+
 import * as db from '../lib/db';
+import * as auth from '../lib/auth';
 import { currentDate } from '../lib/date';
 
 const router = express.Router();
@@ -8,6 +10,16 @@ const router = express.Router();
 router.get('/', async (req, res) => {
   const { rows } = await db.query('SELECT * FROM users');
   res.send(rows);
+});
+
+router.post('/login', async (req, res) => {
+  const { password, email } = req.body;
+  const { rows } = await db.query('select * from users where email = $1', [email]);
+  console.log(rows[0]);
+  if (rows[0].password === auth.getHash(password)) {
+    return res.send('sama');
+  }
+  return res.send('tidak');
 });
 
 router.get('/:id', async (req, res) => {
@@ -21,12 +33,13 @@ router.post('/create-user', async (req, res) => {
     username, email, password, roleId
   } = req.body;
 
+  const resultHash = await auth.getHash(password);
+
+
   const result = await db.insertRows(
     'users(username, email, password, created_on)',
-    'values($1, $2, $3, $4)  RETURNING user_id', [username, email, password, currentDate], res
+    'values($1, $2, $3, $4)  RETURNING user_id', [username, email, resultHash, currentDate], res
   );
-
-  console.log(result);
 
   await db.insertRows(
     'account_role(user_id,role_id,grant_date)',
