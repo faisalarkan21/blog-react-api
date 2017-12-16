521b436bc4c2edf3dc8528d60a5a413ee6a81bb7
faisal arkan <faisalarkan21@gmail.com>
2017-12-16 17:21:30 +0700

    morgan.js into dev mode

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.use(morgan('combined'));
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
